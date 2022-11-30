1af033e39061a704fae56cfddd7540f3829546f4
lov3five <luongtranstaff@gmail.com>
2022-11-30 19:02:49 +0700

    Edit cors

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.static("public", {
 /* app.set('view engine', 'ejs') */
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Homepage');
+    res.send('Welcome to Naker UI');
 });
 
 // Router
